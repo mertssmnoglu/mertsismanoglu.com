1bab1615d773493604197e9571f36bf6d15f5185
Mert Şişmanoğlu <mert190737fb@gmail.com>
2022-07-22 00:41:06 +0300

    Create 'Projects View' with basic heading

---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <Heading message="My Projects"/>
+</template>
+
+<script>
+// @ is an alias to /src
+import Heading from '@/components/Heading.vue'
+
+export default {
+  name: 'ProjectsView',
+  components: {
+    Heading
+  }
+}
+</script>
