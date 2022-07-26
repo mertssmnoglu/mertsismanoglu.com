eb68ace0d0b7d11e55c4d936f3797be0b18e0326
Mert Şişmanoğlu <mert190737fb@gmail.com>
2022-07-26 06:34:15 +0300

    Update HomeView with ProfileCard component.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
   <Heading message="Mert Şişmanoğlu"/>
+  <ProfileCard/>
 </template>
 
 <script>
 // @ is an alias to /src
 import Heading from '@/components/Heading.vue'
+import ProfileCard from '@/components/ProfileCard.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    Heading
+    Heading,
+    ProfileCard
   }
 }
 </script>
