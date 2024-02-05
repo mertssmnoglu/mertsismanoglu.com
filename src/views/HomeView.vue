4cefd493a84260a7b3d9467cdf08c7de2349d85b
Mert Şişmanoğlu <mert190737fb@gmail.com>
2024-02-05 16:57:09 +0300

    Chore: Update HomeView component import in router index.js

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-    <main>
-        <TheWelcome />
-    </main>
+    <div class="home">
+        <h1>Home sweet home</h1>
+    </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+    .home {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
